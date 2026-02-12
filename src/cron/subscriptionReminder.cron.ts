73f8425047842b8a6ca8e74348c70e953792efbe
Vanessa Pisati <pisati.vanessa@gmail.com>
2026-02-12 09:18:13 +0100

    Setted cron timing after test

--- src/cron/subscriptionReminder.cron.ts
+++ src/cron/subscriptionReminder.cron.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import { sendWhatsappMessage } from '../services/whatsapp.service';
 import { Subscription } from '../models/subscription.model';
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 9 * * *', async () => {
 
   console.log('⏰ Cron WhatsApp avviato');
 
